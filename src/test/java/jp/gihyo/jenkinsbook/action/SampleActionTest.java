53a6a3b0ff58aaf0df5752401c0838034b2bc0f8
sjm <swoosnoo@gmail.com>
2019-04-08 23:00:16 +0900

    テスト修正

--- src/test/java/jp/gihyo/jenkinsbook/action/SampleActionTest.java
+++ src/test/java/jp/gihyo/jenkinsbook/action/SampleActionTest.java
@@ -81,20 +81,7 @@ public class SampleActionTest {
 		SampleAction action = new SampleAction();
 		HttpServletRequest request = createMock(HttpServletRequest.class);
 
-		expect(request.getParameter("LastName")).andReturn(null);
-
-		replay(request);
-		boolean result = action.checkParameter(request);
-		verify(request);
-
-		assertEquals(false, result);
-	}
-
-	@Test
-	public void testCheckError5() {
-		SampleAction action = new SampleAction();
-		HttpServletRequest request = createMock(HttpServletRequest.class);
-
+		expect(request.getParameter("FirstName")).andReturn("firstName");
 		expect(request.getParameter("LastName")).andReturn("");
 
 		replay(request);
